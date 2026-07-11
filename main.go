50a249d027f8f9b5a57aa9a023dd2af871c72ba7
Lucas Simpson <lucas.simpson0213@gmail.com>
2026-07-10 21:20:53 -0500

    added go root handler and typescript comp

--- main.go
+++ main.go
@@ -2,11 +2,27 @@ package main
 
 import (
 	"fmt"
+
 	// "log"
-	// "net/http"
+
+	"html/template"
+	"net/http"
 )
 
-func main() {
-	// fmt.Println("hello")
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
+var templates = template.Must(template.ParseFiles("frontend/html/index.html"))
+
+func main() {
+	fmt.Println("hello")
+	http.HandleFunc("/", rootHandler)
+
+}
+
+func rootHandler(rw http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(rw, "index.html", nil)
 }
--- scripts/build-ts-and-go.sh
+++ scripts/build-ts-and-go.sh
@@ -3,7 +3,12 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 build_ts() {
+ cd ~/DEV/homelab_dashboard/frontend
  tsc --no-emit
+}
+
+build_go() {
+
 }
 main() {
 
