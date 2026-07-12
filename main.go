1346f6e760165078b492d400b4a95104c9d62def
Lucas Simpson <lucas.simpson0213@gmail.com>
2026-07-12 13:23:37 -0500

    added static path and making assets be returned properly such as js, css and html

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
 
 	// "log"
-
+	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 )
 
 func check(e error) {
@@ -15,14 +15,34 @@ func check(e error) {
 	}
 }
 
-var templates = template.Must(template.ParseFiles("frontend/html/index.html"))
+var templates = template.Must(template.ParseFiles("dist/index.html"))
 
-func main() {
-	fmt.Println("hello")
-	http.HandleFunc("/", rootHandler)
-
+type PageData struct {
+	Path string
 }
 
+func main() {
+	workingdir, err := os.Getwd()
+	check(err)
+
+	fmt.Println("working dir", workingdir)
+	http.HandleFunc("/dist/", staticHandler)
+	http.HandleFunc("/", rootHandler)
+	error := http.ListenAndServe(":8080", nil)
+	check(error)
+}
+func staticHandler(rw http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	fmt.Println("path: ", path)
+	dat := PageData{
+		Path: path,
+	}
+
+	if err := templates.ExecuteTemplate(rw, "index.html", dat); err != nil {
+		http.Error(rw, "failed to render page", http.StatusInternalServerError)
+	}
+
+}
 func rootHandler(rw http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(rw, "index.html", nil)
 }
--- scripts/build-ts-and-go.sh
+++ scripts/build-ts-and-go.sh
@@ -3,14 +3,18 @@ set -Eeuo pipefail
 IFS=$'\n\t'
 
 build_ts() {
- cd ~/DEV/homelab_dashboard/frontend
- tsc --no-emit
+ cd ~/DEV/homelab-dashboard/frontend/
+ npm run  build
 }
 
 build_go() {
-
+    go run ~/DEV/homelab-dashboard/main.go
 }
 main() {
+
+
+        build_ts
+        build_go
 
 }
 
